3122066ce2e3028c87e158e415b73e236601f7e1
UjjwalAglawe <127344565+UjjwalAglawe@users.noreply.github.com>
2024-05-03 10:04:25 +0530

    Adding contract

---
+++ contract/Videp.sol
@@ -0,0 +1,112 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.5.0;
+
+// import "./TRC721.sol"; // Import the TRC721 contract
+
+contract NFTMarketplace {
+    uint public itemCount;
+    uint public tokenCount;
+
+    mapping(uint => string) public tokenURIs; // Mapping from token ID to token URI
+
+    struct Item {
+        address ogOwner;
+        uint itemId;
+        uint tokenId;
+        uint256 price;
+        address payable seller;
+        bool sold;
+    }
+
+    mapping(uint => Item) public items;
+
+    event Offered(
+        uint itemId,
+        uint tokenId,
+        uint256 price,
+        address indexed seller
+    );
+
+    event Bought(
+        uint itemId,
+        uint tokenId,
+        uint256 price,
+        address indexed seller,
+        address indexed buyer
+    );
+
+    constructor() public {}
+
+    function mint(address _to, uint256 _tokenId, string memory _tokenURI, uint _price) public {
+        itemCount++;
+        tokenCount++;
+        items[itemCount] = Item(
+            _to,
+            itemCount,
+            _tokenId,
+            _price,
+            msg.sender,
+            false
+        );
+
+        tokenURIs[_tokenId] = _tokenURI; // Set the token URI for the minted token
+
+        emit Offered(
+            itemCount,
+            _tokenId,
+            _price,
+            msg.sender
+        );
+    }
+
+    function purchaseItem(uint _itemId) external payable {
+        uint256 _totalPrice = getTotalPrice(_itemId);
+        Item storage item = items[_itemId];
+        require(_itemId > 0 && _itemId <= itemCount, "Item doesn't exist");
+        require(msg.value >= _totalPrice, "Not enough TRX to cover item price");
+        require(!item.sold, "Item already sold");
+
+        item.sold = true;
+
+        address payable temp = item.seller;
+        item.seller = msg.sender;
+
+        temp.transfer(item.price); // Transfer the payment to the seller
+
+        emit Bought(
+            _itemId,
+            item.tokenId,
+            item.price,
+            temp,
+            msg.sender
+        );
+    }
+
+    function getTotalPrice(uint _itemId) view public returns(uint256) {
+        return ((items[_itemId].price * (100 + 2)) / 100); // Add market fee of 2%
+    }
+
+    function getOwner(uint _itemId) view public returns(address) {
+        address ogOwner = items[_itemId].ogOwner;
+        return ogOwner;
+    }
+
+    function getCurrentOwner(uint _itemId) view public returns(address) {
+        // address currOwner = ownerOf(items[_itemId].tokenId);
+                address currOwner = items[_itemId].seller;
+
+        return currOwner;
+    }
+
+    function seeNFT(uint _itemId) external view returns(string memory) {
+        string memory uri = tokenURIs[items[_itemId].tokenId]; // Retrieve URI from mapping
+        return uri;
+    }
+
+    // Implement remaining functions as needed
+
+    // Dummy implementation of ownerOf function to make the contract compile
+    // function ownerOf(uint256 tokenId) internal view returns (address) {
+    //     return address(0);
+    // }
+}
---
+++ contract/abi.js
@@ -0,0 +1,302 @@
+[
+	{
+		"inputs": [],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "constructor"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": false,
+				"internalType": "uint256",
+				"name": "itemId",
+				"type": "uint256"
+			},
+			{
+				"indexed": false,
+				"internalType": "uint256",
+				"name": "tokenId",
+				"type": "uint256"
+			},
+			{
+				"indexed": false,
+				"internalType": "uint256",
+				"name": "price",
+				"type": "uint256"
+			},
+			{
+				"indexed": true,
+				"internalType": "address",
+				"name": "seller",
+				"type": "address"
+			},
+			{
+				"indexed": true,
+				"internalType": "address",
+				"name": "buyer",
+				"type": "address"
+			}
+		],
+		"name": "Bought",
+		"type": "event"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": false,
+				"internalType": "uint256",
+				"name": "itemId",
+				"type": "uint256"
+			},
+			{
+				"indexed": false,
+				"internalType": "uint256",
+				"name": "tokenId",
+				"type": "uint256"
+			},
+			{
+				"indexed": false,
+				"internalType": "uint256",
+				"name": "price",
+				"type": "uint256"
+			},
+			{
+				"indexed": true,
+				"internalType": "address",
+				"name": "seller",
+				"type": "address"
+			}
+		],
+		"name": "Offered",
+		"type": "event"
+	},
+	{
+		"constant": true,
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "_itemId",
+				"type": "uint256"
+			}
+		],
+		"name": "getCurrentOwner",
+		"outputs": [
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "_itemId",
+				"type": "uint256"
+			}
+		],
+		"name": "getOwner",
+		"outputs": [
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "_itemId",
+				"type": "uint256"
+			}
+		],
+		"name": "getTotalPrice",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [],
+		"name": "itemCount",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"name": "items",
+		"outputs": [
+			{
+				"internalType": "address",
+				"name": "ogOwner",
+				"type": "address"
+			},
+			{
+				"internalType": "uint256",
+				"name": "itemId",
+				"type": "uint256"
+			},
+			{
+				"internalType": "uint256",
+				"name": "tokenId",
+				"type": "uint256"
+			},
+			{
+				"internalType": "uint256",
+				"name": "price",
+				"type": "uint256"
+			},
+			{
+				"internalType": "address payable",
+				"name": "seller",
+				"type": "address"
+			},
+			{
+				"internalType": "bool",
+				"name": "sold",
+				"type": "bool"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": false,
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "_to",
+				"type": "address"
+			},
+			{
+				"internalType": "uint256",
+				"name": "_tokenId",
+				"type": "uint256"
+			},
+			{
+				"internalType": "string",
+				"name": "_tokenURI",
+				"type": "string"
+			},
+			{
+				"internalType": "uint256",
+				"name": "_price",
+				"type": "uint256"
+			}
+		],
+		"name": "mint",
+		"outputs": [],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"constant": false,
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "_itemId",
+				"type": "uint256"
+			}
+		],
+		"name": "purchaseItem",
+		"outputs": [],
+		"payable": true,
+		"stateMutability": "payable",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "_itemId",
+				"type": "uint256"
+			}
+		],
+		"name": "seeNFT",
+		"outputs": [
+			{
+				"internalType": "string",
+				"name": "",
+				"type": "string"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [],
+		"name": "tokenCount",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"name": "tokenURIs",
+		"outputs": [
+			{
+				"internalType": "string",
+				"name": "",
+				"type": "string"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	}
+]
